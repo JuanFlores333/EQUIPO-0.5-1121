aeacd6c9f6afd779b5cf99bc0987296934ab2337
JuanFlores333 <89995861+JuanFlores333@users.noreply.github.com>
2021-09-22 13:32:08 -0500

    Add files via upload

---
+++ palabras.js
@@ -0,0 +1,15 @@
+var i = prompt("ingrese un numero: ")
+function makeid(length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+       result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+ }
+ 
+ console.log(makeid(i));
+ for (let l = 1; l < i; l++){
+     console.log(makeid(i));
+ }
